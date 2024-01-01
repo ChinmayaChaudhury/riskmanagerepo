a59ff2fa71e92d10a6162d021618470c2080ef3d
I326821 <chinmaya.gorachanda.chaudhury@sap.com>
2024-01-01 13:37:57 +0000

    2nd commit

---
+++ app/uimodule/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","btp/uimodule/model/models"],function(e,t,i){"use strict";return e.extend("btp.uimodule.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(i.createDeviceModel(),"device")}})});
---
+++ app/uimodule/dist/controller/firstView.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(e){"use strict";return e.extend("btp.uimodule.controller.firstView",{onInit:function(){}})});
